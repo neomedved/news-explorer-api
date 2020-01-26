3b69d885ed10c306b9d64f54894cf6a981a64d9b
Artem Medvedev <at.medvedev@gmail.com>
2020-01-26 11:36:27 +0300

    mlab-reverse

--- config/mongo-url.js
+++ config/mongo-url.js
@@ -1,3 +1,1 @@
-const { NODE_ENV, MONGODB_URI } = process.env;
-
-module.exports = NODE_ENV === 'production' ? MONGODB_URI : 'mongodb://localhost:27017/newsdb';
+module.exports = 'mongodb://localhost:27017/newsdb';
